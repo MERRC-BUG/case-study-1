0e1a184fc079644a281dfb6f11f4c6f6c6e8d304
MERRC-BUG <huy74874@gmail.com>
2025-12-23 18:02:17 +0700

    Add files via upload

--- case study/memories card.js
+++ case study/memories card.js
@@ -1,7 +1,48 @@
+
+const cards = document.querySelectorAll('.card');
+let flippedcard = [];
+let matchedcards = [];
+
+function flipCard() {
+      if (flippedcard.length >=2) return;
+      this.classlist.add('flipped');
+      flippedcard.push(this);
+
+      if (flippedcard.length ===2) {
+            checkmatch();
+      }
+}    
+function checkmatch(){
+      const [card1, card2] = flippedcard;
+      const img1 = card1.querySelectorAll('.card-back').style.backsgroundImage;
+      const img2 = card2.querySelectorAll('.card-back').style.backsgroundImage;
+      if (img1 === img2) {
+            matchedcards.push(card1,card2);
+            flippedcard = [];
+            if (matchedcards.length === cards.length) {
+                  setTimeout(() => alert("congratulations! you win!"), 500);
+            }
+            else {
+                  setTimeout(() => {
+                        card1.classlist.remove('flipped');
+                        card2.classlist.remove('flipped');
+                        flippedcard = [];
+                  }, 1000);
+            }
+      }
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
+
 function startGame() {
-      alert("Game started! Good luck to you!");     
+      matchedcards = [];
+      
+      cards.forEach(card => card.classlist.remove('flipped'));
+      shuffleCards();
+}
+function resetGame() {
+      matchedcards = [];
+      flippedcard = [];
+      cards.forEach(card => card.classlist.remove('flipped'));
+      shuffleCards();
 }
 
-function resetGame() {
-      alert("Game reset! please try again.");
-}
